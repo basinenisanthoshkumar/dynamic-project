6f4ed407a305b78041f65051e04901799a36f566
basinenisanthoshkumar <113873434+basinenisanthoshkumar@users.noreply.github.com>
2023-04-22 21:41:55 +0530

    4 files modified

--- script.js
+++ script.js
@@ -10,15 +10,5 @@ function fuu(){
         var branch = document.forms["myForm"]["branch"].value;
         var year = document.forms["myForm"]["year"].value;
     }
-    document.querySelector('form').addEventListener('submit', function(event) {
-      // Prevent the default form submission behavior
-      event.preventDefault();
-      
-      // Get the URL of the next page
-      var nextPageUrl = 'https://web-platform-zb4tot.stackblitz.io/page1.html'; // Replace with the URL of your next page
-      
-      // Navigate to the next page
-      window.location.href = nextPageUrl;
-    });
     
     
