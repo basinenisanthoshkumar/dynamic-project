03b275db7852ade52abcc5ae19f73ef743c494aa
basinenisanthoshkumar <113873434+basinenisanthoshkumar@users.noreply.github.com>
2023-04-25 17:35:51 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 function validateForm() {
-        var firstname = document.forms["myForm"]["firstname"].value;
-       localStorage.setItem("FirstName",firstname);
+  var firstname = document.forms['myForm']['firstname'].value;
+  localStorage.setItem('FirstName', firstname);
 
-        var lastname = document.forms["myForm"]["lastname"].value;
-       localStorage.setItem("LastName",firstname);
+  var lastname = document.forms['myForm']['lastname'].value;
+  localStorage.setItem('LastName', firstname);
 
-        var Email= document.forms["myForm"]["email"].value;
-        localStorage.setItem("Email",Email);
+  var Email = document.forms['myForm']['email'].value;
+  localStorage.setItem('Email', Email);
 
-        var Number = document.forms["myForm"]["number"].value;
-        localStorage.setItem("Number",Number);
+  var Number = document.forms['myForm']['number'].value;
+  localStorage.setItem('Number', Number);
 
-        var dob = document.forms["myForm"]["dob"].value;
-        localStorage.setItem("Dob",dob);
+  var dob = document.forms['myForm']['dob'].value;
+  localStorage.setItem('Dob', dob);
 
-        var address = document.forms["myForm"]["address"].value;
-       localStorage.setItem("Address",address);
+  var address = document.forms['myForm']['address'].value;
+  localStorage.setItem('Address', address);
 
-        var pincode = document.forms["myForm"]["pincode"].value;
-        localStorage.setItem("Pincode",pincode);
+  var pincode = document.forms['myForm']['pincode'].value;
+  localStorage.setItem('Pincode', pincode);
 
-        var branch = document.forms["myForm"]["branch"].value;
-        localStorage.setItem("Branch",branch);
+  var branch = document.forms['myForm']['branch'].value;
+  localStorage.setItem('Branch', branch);
 
-        var year = document.forms["myForm"]["year"].value;
-        localStorage.setItem("Year",year);
-    }
-    
-    document.getElementById("header").innerHTML=localStorage.getItem('Userid');
-    
-    function loginPage() {
+  var year = document.forms['myForm']['year'].value;
+  localStorage.setItem('Year', year);
+}
 
-          if (!localStorage.getItem('Userid')) {   
-            window.location.href = 'index.html';      
-          }       
-    }
-        window.onload = loginPage;
+document.getElementById('header').innerHTML = localStorage.getItem('Userid');
+
+function loginPage() {
+  if (!localStorage.getItem('Userid')) {
+    window.location.href = 'index.html';
+  }
+}
+window.onload = loginPage;
+
+let button = document.getElementById('logOut');
+button.addEventListener('click', logout);
 
 function logout() {
-     localStorage.removeItem("Userid");
-     localStorage.removeItem("Password");
-      call(loginPage());
-     //window.open("index.html");
-      //window.location.href = "index.html";
+  localStorage.removeItem('Userid');
+  localStorage.removeItem('Password');
+  window.location.href = 'index.html';
 }
